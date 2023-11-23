d0bd5a616011c44e06fb6499e5aca3fdc7102a8a
Oliancem <117437190+Oliancem@users.noreply.github.com>
2023-11-23 13:15:13 +0100

    Update Code-Arduino.ino

--- Code-Arduino.ino
+++ Code-Arduino.ino
@@ -3,24 +3,34 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
 
-#define SSID    "Name_Wifi"
-#define PASSWD  "Password"
+#define SSID    "dLonelyPrince"
+#define PASSWD  "prince44"
 
-#define SS_PIN  3
+#define SS_PIN  4
 #define RST_PIN 27 
+
+#define RED_LED_PIN  3
+#define GREEN_LED_PIN 2
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(115200);
   SPI.begin();
-  rfid.PCD_Init(); 
+  rfid.PCD_Init();
+
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+
+  digitalWrite(RED_LED_PIN, HIGH); 
+
   WiFi.begin(SSID, PASSWD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.println("Connecting to WiFi...");
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
   Serial.println("Connected to WiFi");
+
 
   Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader : ");
 }
@@ -37,12 +47,14 @@ void loop() {
       Serial.print("UID: ");
       char uidString[25];  
 
+      // Create a String that contains "AABBCCFF"
       sprintf(uidString, "%02X%02X%02X%02X", 
               rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2],
               rfid.uid.uidByte[3]);
+      
       Serial.println(uidString);
 
-      String url = "your_adress_ip?uid=" + String(uidString);
+      String url = "https://134.209.84.201/index.php?uid=" + String(uidString);
 
       Serial.println("Constructed URL: " + url);
 
@@ -53,8 +65,21 @@ void loop() {
       if (httpCode > 0) {
         String payload = http.getString();  
         Serial.println("Server response: " + payload);
+
+        // The API returns "ACCESS GRANTED" or "ACCESS DENIED"
+        if (payload.indexOf("ACCESS GRANTED") != -1) {
+          digitalWrite(GREEN_LED_PIN, HIGH);  
+          digitalWrite(RED_LED_PIN, LOW);    
+        } else {
+          digitalWrite(RED_LED_PIN, HIGH);   
+          digitalWrite(GREEN_LED_PIN, LOW);  
+        }
       } else {
         Serial.println("HTTP request failed");
+
+        digitalWrite(RED_LED_PIN, LOW);
+        delay(100);  
+        digitalWrite(RED_LED_PIN, HIGH);  
       }
 
       http.end();  
@@ -63,4 +88,4 @@ void loop() {
       rfid.PCD_StopCrypto1(); 
     }
   }
-} 
+}
